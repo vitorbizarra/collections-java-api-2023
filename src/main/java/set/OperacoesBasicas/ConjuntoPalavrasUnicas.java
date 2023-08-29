f1d1e46a715cf56f64390bc2673837680e4aeb66
vitorbizarra <vitorbizarracontato@gmail.com>
2023-08-28 22:23:41 -0300

    :sparkles: feat: Resolução exercício Conjunto de Palavras Únicas

--- src/main/java/set/OperacoesBasicas/ConjuntoPalavrasUnicas.java
+++ src/main/java/set/OperacoesBasicas/ConjuntoPalavrasUnicas.java
@@ -4,35 +4,57 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class ConjuntoPalavrasUnicas {
-    
+  private Set<String> wordSet;
+
+  public ConjuntoPalavrasUnicas() {
+    this.wordSet = new HashSet<>();
+  }
+
+  public void adicionarPalavra(String palavra) {
+    this.wordSet.add(palavra);
+  }
+
+  public void removerPalavra(String palavra) {
+    this.wordSet.remove(palavra);
+  }
+
+  public boolean verificarPalavra(String palavra) {
+    return this.wordSet.contains(palavra);
+  }
+
+  public void exibirPalavrasUnicas() {
+    System.out.println(this.wordSet);
+  }
 
   public static void main(String[] args) {
-    // // Criando uma instância da classe ConjuntoPalavrasUnicas
-    // ConjuntoPalavrasUnicas conjuntoLinguagens = new ConjuntoPalavrasUnicas();
+    // Criando uma instância da classe ConjuntoPalavrasUnicas
+    ConjuntoPalavrasUnicas conjuntoLinguagens = new ConjuntoPalavrasUnicas();
 
-    // // Adicionando linguagens únicas ao conjunto
-    // conjuntoLinguagens.adicionarPalavra("Java");
-    // conjuntoLinguagens.adicionarPalavra("Python");
-    // conjuntoLinguagens.adicionarPalavra("JavaScript");
-    // conjuntoLinguagens.adicionarPalavra("Python");
-    // conjuntoLinguagens.adicionarPalavra("C++");
-    // conjuntoLinguagens.adicionarPalavra("Ruby");
+    // Adicionando linguagens únicas ao conjunto
+    conjuntoLinguagens.adicionarPalavra("Java");
+    conjuntoLinguagens.adicionarPalavra("Python");
+    conjuntoLinguagens.adicionarPalavra("JavaScript");
+    conjuntoLinguagens.adicionarPalavra("Python");
+    conjuntoLinguagens.adicionarPalavra("C++");
+    conjuntoLinguagens.adicionarPalavra("Ruby");
 
-    // // Exibindo as linguagens únicas no conjunto
-    // conjuntoLinguagens.exibirPalavrasUnicas();
+    // Exibindo as linguagens únicas no conjunto
+    conjuntoLinguagens.exibirPalavrasUnicas();
 
-    // // Removendo uma linguagem do conjunto
-    // conjuntoLinguagens.removerPalavra("Python");
-    // conjuntoLinguagens.exibirPalavrasUnicas();
+    // Removendo uma linguagem do conjunto
+    conjuntoLinguagens.removerPalavra("Python");
+    conjuntoLinguagens.exibirPalavrasUnicas();
 
-    // // Removendo uma linguagem inexistente
-    // conjuntoLinguagens.removerPalavra("Swift");
+    // Removendo uma linguagem inexistente
+    conjuntoLinguagens.removerPalavra("Swift");
 
-    // // Verificando se uma linguagem está no conjunto
-    // System.out.println("A linguagem 'Java' está no conjunto? " + conjuntoLinguagens.verificarPalavra("Java"));
-    // System.out.println("A linguagem 'Python' está no conjunto? " + conjuntoLinguagens.verificarPalavra("Python"));
+    // Verificando se uma linguagem está no conjunto
+    System.out.println("A linguagem 'Java' está no conjunto? " +
+        conjuntoLinguagens.verificarPalavra("Java"));
+    System.out.println("A linguagem 'Python' está no conjunto? " +
+        conjuntoLinguagens.verificarPalavra("Python"));
 
-    // // Exibindo as linguagens únicas atualizadas no conjunto
-    // conjuntoLinguagens.exibirPalavrasUnicas();
+    // Exibindo as linguagens únicas atualizadas no conjunto
+    conjuntoLinguagens.exibirPalavrasUnicas();
   }
 }
